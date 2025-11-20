35c8b58f04f3828baf086a0360ed4130f320b43c
Sartaj <sartaj@sartaj.me>
2025-11-20 02:11:17 -0600

    Update

--- actions/from-windy-civi-pipelines/render_snapshot.sh
+++ actions/from-windy-civi-pipelines/render_snapshot.sh
@@ -2,11 +2,10 @@
 
 SNAPSHOTS_DIR="__snapshots__"
 OUTPUT_FILE="$SNAPSHOTS_DIR/output.log"
-GIT_DIR="tmp/git/windy-civi-pipelines"
+GIT_DIR="../sync-windy-civi-pipelines/__snapshots__"
 
 # Create snapshots directory
 rm -rf "$SNAPSHOTS_DIR"
 mkdir -p "$SNAPSHOTS_DIR"
 
-./tools/git-sync-repos.sh --git-dir $GIT_DIR usa il
-./tools/get-logs.sh --git-dir $GIT_DIR --output $OUTPUT_FILE --sort DESC --limit 100
+./main.sh --git-dir $GIT_DIR --output $OUTPUT_FILE --sort DESC --limit 100
