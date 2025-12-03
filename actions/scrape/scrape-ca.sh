b592a99ab95dffb05ebad66dea22f2acfdb4810c
Tamara Dowis <tamara.dowis@gmail.com>
2025-12-02 21:51:48 -0600

    Install sqlalchemy 1.x for CA scraper compatibility

--- actions/scrape/scrape-ca.sh
+++ actions/scrape/scrape-ca.sh
@@ -53,7 +53,7 @@ if docker run --rm \
   -v "$(pwd)/_working/_cache":/opt/openstates/openstates/_cache \
   --entrypoint /bin/bash \
   openstates/scrapers:${DOCKER_IMAGE_TAG} \
-  -c "/root/.cache/pypoetry/virtualenvs/*/bin/pip install sqlalchemy pymysql && /root/.cache/pypoetry/virtualenvs/*/bin/os-update ca bills --scrape --fastmode" 2>&1 | tee -a "$SCRAPE_LOG"
+  -c "/root/.cache/pypoetry/virtualenvs/*/bin/pip install 'sqlalchemy<2.0' pymysql && /root/.cache/pypoetry/virtualenvs/*/bin/os-update ca bills --scrape --fastmode" 2>&1 | tee -a "$SCRAPE_LOG"
 then
   echo "✅ California scrape completed" | tee -a "$SCRAPE_LOG"
 else
